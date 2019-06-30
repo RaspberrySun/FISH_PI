40f3804d36d189e576178d876f478df234c6ba82
RaspberrySun <40199176+RaspberrySun@users.noreply.github.com>
2019-06-30 21:30:49 +0800

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,46 +1,39 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN)
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(0, GPIO.IN)
-GPIO.setup(0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(2, GPIO.IN)
-GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(3, GPIO.IN)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(21, GPIO.IN)
-GPIO.setup(21,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(22, GPIO.IN)
-GPIO.setup(22,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+PinList = [7, 11, 13, 15, 29, 31]
+GPIO.setup(PinList, GPIO.IN)
+GPIO.setup(PinList, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 def read_data1():
 	if GPIO.input(7):
 		return 0
 	else:
 		return 1
 def read_data2():
-	if GPIO.input(0):
+	if GPIO.input(11):
 		return 0
 	else:
 		return 1
 def read_data3():
-	if GPIO.input(2):
+	if GPIO.input(13):
 		return 0
 	else:
 		return 1
 def read_data4():
-	if GPIO.input(3):
+	if GPIO.input(15):
 		return 0
 	else:
 		return 1
 def read_data5():
-	if GPIO.input(21):
+	if GPIO.input(29):
 		return 0
 	else:
 		return 1
 def read_data6():
-	if GPIO.input(22):
+	if GPIO.input(31):
 		return 0
 	else:
 		return 1
-while 1:
-	read_data1()
+
+if __name__ == "__main__":
+	print [read_data1(), read_data2(), read_data3(), read_data4(), read_data5(), read_data6()]
